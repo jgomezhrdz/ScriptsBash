bee4cda008ca622d1e8014e54277cb1a889cb8ad
Juan <jgomez.hrdz@gmail.com>
2021-12-19 15:39:47 +0100

    moficiacion del ejercicio 3

--- Ejercicio3.sh
+++ Ejercicio3.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 argv=("$@")
+echo "El archivo en ejecución es ${0}"
+echo "El número de argumentos es ${#}"
 
-echo "El archivo en ejecución es: ${0}"
-echo "El número de argumentos es: ${#}"
+echo "Argumentos: ${1} ${2}"
 
-echo "Argumentos (1 y 2): ${1} ${2}"
-
-for i in $argv; do
-    echo $i
+for (( i=2; i < $#; i++ )); do
+    echo ${argv[${i}]}
 done
